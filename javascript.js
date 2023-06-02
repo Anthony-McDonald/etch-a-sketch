492f6b055a8803537fbd43e92f5a135a35304f7f
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2023-06-02 16:48:49 +0100

    completed etchh a sketch interface

--- javascript.js
+++ javascript.js
@@ -2,20 +2,50 @@
 
 const gridContainer = document.querySelector('gridContainer');
 
-for (let i = 0; i < 16; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.className = 'gridBox';
-    gridBox.id = 'block';
-
-    for (let j = 0; j < 16; j++) {
-        const subBox = document.createElement('div');
-        subBox.className = 'subBox'
-        subBox.id = 'block';
-        gridBox.appendChild(subBox);
+function gridCreator(size) {
+    if (size >= 100) {
+        return 0;
+    } else {
+        gridEmptier()
     }
-    document.getElementsByClassName('gridContainer')[0].appendChild(gridBox);
+
+    for (let i = 0; i < size; i++) {
+        const gridBox = document.createElement('div');
+        gridBox.className = 'gridBox';
+        gridBox.id = 'block';
+    
+        for (let j = 0; j < size; j++) {
+            const subBox = document.createElement('div');
+            subBox.className = 'subBox'
+            subBox.id = 'block';
+            gridBox.appendChild(subBox);
+        }
+        document.getElementsByClassName('gridContainer')[0].appendChild(gridBox);
+
+        // Hover over functionality
+        document.querySelectorAll('.subBox').forEach(item => {
+            item.addEventListener('mouseover', event => {
+              item.classList.add('hoveredOver');
+            })
+            // item.addEventListener('mouseout', event => {
+            //     item.classList.remove('hoveredOver');
+            // })
+          })
+    }
 }
 
 
+function gridEmptier() {
+    document.querySelectorAll('#block').forEach(item => {
+        item.remove();
+    })
+}
+  
 
-    
+
+
+  document.querySelector('.newGridButton').addEventListener('click', event => {
+    gridCreator(prompt("How big do you want it to be?"));
+  })
+
+gridCreator(16);
